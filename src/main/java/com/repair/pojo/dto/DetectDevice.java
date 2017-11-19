2744f8bd5668190d92540b4f69359c541a95ef7e
XDSTY <17859735221@163.com>
2017-11-19 21:25:02 +0800

    忽略.class
    忽略.class

--- src/main/java/com/repair/pojo/dto/DetectDevice.java
+++ src/main/java/com/repair/pojo/dto/DetectDevice.java
@@ -10,22 +10,7 @@ public class DetectDevice {
 
     private String mark;
 
-    public DetectDevice() {<!-- 配置Spring mvc -->
-	<!-- 开启Spring mvc注解模式 -->
-	<mvc:annotation-driven></mvc:annotation-driven>
-
-	<!-- 加入对静态资源的处理   允许使用/做资源配置-->
-	<mvc:default-servlet-handler/>
-
-	<!-- 配置jsp显示ViewEwslover -->
-	<bean class="org.springframework.web.servlet.view.InternalResourceViewResolver">
-		<property name="viewClass" value="org.springframework.web.servlet.view.JstlView"></property>
-		<property name="prefix" value="/WEB-INF/jsp/"></property>
-		<property name="suffix" value=".jsp"></property>
-	</bean>
-
-	<!-- 扫描web相关的包 -->
-	<context:component-scan base-package="org.seckill.web"></context:component-scan>
+    public DetectDevice() {
     }
 
     public DetectDevice(String name, Integer state, String mark) {
