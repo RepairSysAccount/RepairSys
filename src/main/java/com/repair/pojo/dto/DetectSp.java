ac778a3d25f930f19df8a64e635ee3e7e1d67fbd
Lighters_c <zyh13227@163.com>
2017-11-19 17:51:15 +0800

    补上巡查类

---
+++ src/main/java/com/repair/pojo/dto/DetectSp.java
@@ -0,0 +1,31 @@
+package com.repair.pojo.dto;
+
+public class DetectSp {
+    private String firstAddr;
+    private String secondAddr;
+    private String deviceClass;
+
+    public String getFirstAddr() {
+        return firstAddr;
+    }
+
+    public void setFirstAddr(String firstAddr) {
+        this.firstAddr = firstAddr;
+    }
+
+    public String getSecondAddr() {
+        return secondAddr;
+    }
+
+    public void setSecondAddr(String secondAddr) {
+        this.secondAddr = secondAddr;
+    }
+
+    public String getDeviceClass() {
+        return deviceClass;
+    }
+
+    public void setDeviceClass(String deviceClass) {
+        this.deviceClass = deviceClass;
+    }
+}
