af4722bf78313236136d6da44b4da2fbf1590eb5
Lighters_c <zyh13227@163.com>
2017-11-19 16:42:41 +0800

    改为连接Sql server

--- src/main/test/com/repair/dao/FirstAddrMapperTest.java
+++ src/main/test/com/repair/dao/FirstAddrMapperTest.java
@@ -8,16 +8,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
+import static org.junit.Assert.*;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath*:spring/spring-*.xml")
+@ContextConfiguration(locations = {"classpath:spring/spring-dao.xml"})
 public class FirstAddrMapperTest {
     @Resource
     private FirstAddrMapper firstAddrMapper;
-
-
     @Test
     public void selectByPrimaryKey() throws Exception {
-        FirstAddr firstAddr=firstAddrMapper.selectByPrimaryKey(1);
+        FirstAddr firstAddr=firstAddrMapper.selectByPrimaryKey(1000);
         System.out.println(firstAddr.getName());
     }
 
