aa82cd2fcdb6348f610cd179613389af004aee09
Lighters_c <zyh13227@163.com>
2017-11-20 08:42:10 +0800

    完成了显示设备列表的接口

---
+++ src/main/test/com/repair/dao/DeviceClassMapperTest.java
@@ -0,0 +1,27 @@
+package com.repair.dao;
+
+import com.repair.pojo.DeviceClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/spring-dao.xml"})
+public class DeviceClassMapperTest {
+    @Resource
+    private DeviceClassMapper deviceClassMapper;
+    @Test
+    public void selectAllDevices() throws Exception {
+        List<DeviceClass> list=deviceClassMapper.selectAllDevices();
+        for(DeviceClass deviceClass:list) {
+            System.out.println(deviceClass);
+        }
+    }
+
+}
